bc737c08ff01f8a1846e19de59cb84bc1d45965c
Kevin Bernfeld <kbernfeld@ngpvan.com>
2019-09-12 14:42:22 -0400

    storybook lololol

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,20 @@
-import { configure } from '@storybook/react';
+import { configure, addParameters } from '@storybook/react';
+import { create } from '@storybook/theming';
+
+addParameters({
+  options: {
+    theme: create({
+      base: 'dark',
+      brandTitle: 'Aiti'
+    })
+  }
+});
 
 // automatically import all files ending in *.stories.js
-configure(require.context('../stories', true, /\.stories\.js$/), module);
+configure([
+  require.context('../stories/Setting', true, /\.mdx$/),
+  require.context('../stories/Races', true, /\.mdx$/),
+  require.context('../stories/Classes', true, /\.mdx$/),
+  require.context('../stories/Spells', true, /\.mdx$/),
+  require.context('../stories/Tables', true, /\.mdx$/),
+], module);
