f873943fc57d0d29deb1c58e4488967ab45e62fc
Kevin Bernfeld <kcbernfeld@gmail.com>
2019-09-12 20:55:33 -0400

    factions, re-arranging

--- .storybook/config.js
+++ .storybook/config.js
@@ -12,11 +12,11 @@ addParameters({
 
 import 'bootstrap/dist/css/bootstrap.css';
 
-// automatically import all files ending in *.stories.js
 configure([
+  require.context('../stories/Intro', true, /\.mdx$/),
   require.context('../stories/Setting', true, /\.mdx$/),
   require.context('../stories/Races', true, /\.mdx$/),
-  require.context('../stories/Classes', true, /\.mdx$/),
+  require.context('../stories/Tweaks', true, /\.mdx$/),
   require.context('../stories/Spells', true, /\.mdx$/),
   require.context('../stories/Tables', true, /\.mdx$/),
 ], module);
